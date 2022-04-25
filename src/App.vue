4ac50f35849684862a47c691913b6bfd0729f800
Alvin <AlvinWang627@gmail.com>
2022-04-25 23:33:39 +0800

    feat: to filter items

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,66 @@
 <script setup>
-import { ref } from 'vue'
-const todos = ref([
-  {
-    title: '學習 Vue Template 的使用',
-    completed: true,
-  },
-  {
-    title: '學習在 Vue Template 中進行條件判斷',
-    completed: false,
-  },
-  {
-    title: '學習在 Vue Template 中使用迴圈',
-    completed: false,
-  }
-])
-</script>
+import { ref, computed } from 'vue';
 
+const todos = ref([]);
+
+const activeTodos = computed(() => {
+  return todos.value.filter((todo) => !todo.completed).length;
+});
+function addTodo(e) {
+  const value = e.target.value.trim();
+  if (value) {
+    todos.value.push({
+      id: Date.now(),
+      title: value,
+      completed: false,
+    });
+    e.target.value = '';
+  }
+}
+function removeTodo(todo) {
+  // console.log(todo)
+  // console.log(todos.value[0].id)
+  // todos.value.splice(todos.value.indexOf(todo), 1)
+  todos.value = todos.value.filter((_todo) => _todo.id !== todo.id);
+}
+</script>
 
 <template>
   <section class="todoapp">
     <header>
       <h1>todos</h1>
     </header>
-    <input type="text" class="new-todo" autofocus placeholder="需要做什麼？" />
-    <main class="main">
+    <input
+      type="text"
+      class="new-todo"
+      autofocus
+      placeholder="需要做什麼？"
+      @keyup.enter="addTodo"
+    />
+    <main class="main" v-show="todos.length !== 0">
       <input id="toggle-all" type="checkbox" class="toggle-all" />
       <label for="toggle-all"></label>
       <ul class="todo-list">
-        <li v-for="todo in todos" class="todo" :class="{ completed: todo.completed }">
+        <li
+          v-for="todo in todos"
+          :key="todo.id"
+          class="todo"
+          :class="{ completed: todo.completed }"
+        >
           <div class="view">
-            <input type="checkbox" class="toggle" />
+            <input v-model="todo.completed" type="checkbox" class="toggle" />
             <label for="">{{ todo.title }}</label>
-            <button class="destroy"></button>
+            <button @click="removeTodo(todo)" class="destroy"></button>
           </div>
           <input type="text" class="edit" />
         </li>
       </ul>
     </main>
-    <footer class="footer">
-      <span class="todo-count"> <strong>3</strong> items left </span>
+    <footer class="footer" v-show="todos.length !== 0">
+      <span class="todo-count">
+        <strong>{{ activeTodos }}</strong>
+        items left
+      </span>
       <ul class="filters">
         <li>
           <a href="#/all">All</a>
