4f99ec3a5104fe33830d1d2bfccb660e617329d3
Alvin <AlvinWang627@gmail.com>
2022-04-26 23:30:48 +0800

    feat:add filter / removeAll feature

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,25 @@
 import { ref, computed } from 'vue';
 
 const todos = ref([]);
+const visibility = ref('all');
+const STORAGE_KEY = 'todomvc-app-vue'
+const filters = {
+  all: (todos) => todos,
+  active: (todos) => todos.filter((todo) => !todo.completed),
+  completed: (todos) => todos.filter((todo) => todo.completed),
+};
 
-const activeTodos = computed(() => {
-  return todos.value.filter((todo) => !todo.completed).length;
+function setVisibility(vs) {
+  console.log(todos.value);
+  visibility.value = vs;
+}
+const filteredTodos = computed(() => {
+  return filters[visibility.value](todos.value);
 });
+const remaining = computed(() => {
+  return filters.active(todos.value).length;
+});
+
 function addTodo(e) {
   const value = e.target.value.trim();
   if (value) {
@@ -18,10 +33,12 @@ function addTodo(e) {
   }
 }
 function removeTodo(todo) {
-  // console.log(todo)
-  // console.log(todos.value[0].id)
   // todos.value.splice(todos.value.indexOf(todo), 1)
   todos.value = todos.value.filter((_todo) => _todo.id !== todo.id);
+}
+
+function removeCompleted() {
+  todos.value = filters.active(todos.value);
 }
 </script>
 
@@ -42,7 +59,7 @@ function removeTodo(todo) {
       <label for="toggle-all"></label>
       <ul class="todo-list">
         <li
-          v-for="todo in todos"
+          v-for="todo in filteredTodos"
           :key="todo.id"
           class="todo"
           :class="{ completed: todo.completed }"
@@ -58,21 +75,42 @@ function removeTodo(todo) {
     </main>
     <footer class="footer" v-show="todos.length !== 0">
       <span class="todo-count">
-        <strong>{{ activeTodos }}</strong>
-        items left
+        <strong>{{ remaining }}</strong>
+        {{ remaining === 1 ? 'item' : 'items' }} left
       </span>
       <ul class="filters">
         <li>
-          <a href="#/all">All</a>
+          <a
+            href="#/all"
+            @click="setVisibility('all')"
+            :class="{ selected: visibility === 'all' }"
+            >All</a
+          >
         </li>
         <li>
-          <a href="#/active">Active</a>
+          <a
+            href="#/active"
+            @click="setVisibility('active')"
+            :class="{ selected: visibility === 'active' }"
+            >Active</a
+          >
         </li>
         <li>
-          <a href="#/completed">Completed</a>
+          <a
+            href="#/completed"
+            @click="setVisibility('completed')"
+            :class="{ selected: visibility === 'completed' }"
+            >Completed</a
+          >
         </li>
       </ul>
-      <button class="clear-completed">Clear completed</button>
+      <button
+        class="clear-completed"
+        @click="removeCompleted"
+        v-show="todos.length > remaining"
+      >
+        Clear completed
+      </button>
     </footer>
   </section>
 </template>
